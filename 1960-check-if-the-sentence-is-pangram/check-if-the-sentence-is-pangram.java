ab536b1248af90b785300078346c761fce49b30b
Parimita Madne <142480294+parimadne@users.noreply.github.com>
2024-02-14 10:05:02 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (13.69%) - LeetSync

---
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        char c ='a';
+        for(int i =0;i<26;i++){
+            if(sentence.contains(String.valueOf(c))){
+                c++;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
